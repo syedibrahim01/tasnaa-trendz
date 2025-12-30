3d21233d8797af56c0532f5b68157b499a12b10d
shajath.syed <58837698+syedibrahim01@users.noreply.github.com>
2025-12-30 12:19:35 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,30 +1,27 @@
-fetch("data/products.json")
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("Failed to load products.json");
-    }
-    return response.json();
+console.log("products.js loaded");
+
+fetch("./data/products.json")
+  .then(res => {
+    console.log("Fetch response:", res.status);
+    return res.json();
   })
-  .then(products => {
+  .then(data => {
+    console.log("Products data:", data);
+
     let html = "";
-    products.forEach(p => {
+    data.forEach(p => {
       html += `
         <div class="card">
-          <img src="${p.image}" alt="${p.name}">
           <h3>${p.name}</h3>
           <p>${p.price}</p>
-          <a class="btn"
-             href="https://wa.me/917550237469"
-             target="_blank">
-             Order on WhatsApp
-          </a>
         </div>
       `;
     });
+
     document.getElementById("product-list").innerHTML = html;
   })
-  .catch(error => {
+  .catch(err => {
+    console.error("ERROR:", err);
     document.getElementById("product-list").innerHTML =
-      "<p style='color:red;text-align:center;'>Products loading failed</p>";
-    console.error(error);
+      "<p style='color:red;text-align:center;'>Failed to load products</p>";
   });
