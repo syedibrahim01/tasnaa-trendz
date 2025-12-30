77a505e4d1fca68005626e3c322e96eb8ea8f8b2
shajath.syed <58837698+syedibrahim01@users.noreply.github.com>
2025-12-30 11:31:15 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,1 +1,12 @@
-
+fetch("data/products.json")
+  .then(res => res.json())
+  .then(data => {
+    let html = "";
+    data.forEach(p => {
+      html += `<div class="card">
+                 <h3>${p.name}</h3>
+                 <p>${p.price}</p>
+               </div>`;
+    });
+    document.getElementById("product-list").innerHTML = html;
+  });
